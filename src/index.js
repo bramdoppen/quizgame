37cdf91df7f0cf70456fbd946572164a5c5294f2
Charly <charlottevswart@gmail.com>
2017-06-02 14:49:36 +0200

    Charlottes component verwijderd

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './homescreen/App';
-import Vraag from './CharlottesComponentje'
 import registerServiceWorker from './registerServiceWorker';
 import './css/main.css';
 import Navigatiebar from "./Navbar";
 
+registerServiceWorker();
+
 let alleComponenten = (
     <div>
-        <App />
-        <Vraag />
         <Navigatiebar />
+        <App />
     </div>
 );
 
 ReactDOM.render(alleComponenten, document.getElementById("root"));
 
-registerServiceWorker();
+
