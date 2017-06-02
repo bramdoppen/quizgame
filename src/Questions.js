f21ccac4f19235350c7b885825b2f741c4203024
Nanco <nancolin92@gmail.com>
2017-06-02 13:34:23 +0200

    v2

---
+++ src/Questions.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import './css/Questions.css';
+
+class Questions extends Component {
+  constructor(props) {
+    super(props);
+    this.answers = this.props.text.answers
+  }
+
+  render() {
+    console.log(this.props.text);
+    console.log(this.props.text.answers[0]);
+    console.log(this.props.questions);
+    return (
+      <div className= 'Questions'>
+        <h1>{this.props.text.question}</h1>
+        <p>{this.props.text.answers[0]}</p>
+        <p>{this.props.text.answers[1]}</p>
+        <p>{this.props.text.answers[2]}</p>
+        <p>{this.props.text.answers[3]}</p>
+      </div>
+    );
+  }
+}
+
+export default Questions;
--- src/homescreen/App.js
+++ src/homescreen/App.js
@@ -1,19 +1,25 @@
 import React, { Component } from 'react';
 import logo from '../img/logo.svg';
+import Question from '../Questions.js';
+
+import { questions } from '../quiz/quiz-data.js';
+
+const QUESTIONS = questions;
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-          <div>
-            <div className="App-header">
-              <img src={logo} className="App-logo" alt="logo" />
-              <h2>Hallo teamgenootjessssss</h2>
-            </div>
-            <p className="App-intro">
-                {this.props.ondertitel}
-            </p>
+        <div>
+          <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>Hallo teamgenootjessssss</h2>
           </div>
+          <p className="App-intro">
+            {this.props.ondertitel}
+          </p>
+        </div>
+        <Question text={QUESTIONS[2]} />
       </div>
     );
   }
