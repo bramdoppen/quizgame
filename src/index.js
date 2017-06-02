fa50fe977a5c124da9df8a72c0a3dc82a544f019
Nanco <nancolin92@gmail.com>
2017-06-02 15:08:14 +0200

    v4

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom';
 import App from './homescreen/App';
 import registerServiceWorker from './registerServiceWorker';
 import './css/main.css';
-//import Navigatiebar from "./Navbar";
+import Navigatiebar from "./Navbar";
 
 registerServiceWorker();
 
 let alleComponenten = (
     <div>
         <App />
+        <Navigatiebar />
     </div>
 );
 
